31e276496043a10229a8dc27959696cf2e01e9fe
janikJ <71125912+janik-1@users.noreply.github.com>
2023-12-03 22:28:06 +0100

    Add Tests

---
+++ src/test/java/fr/efrei/studentserver/web/rest/StudentRessourceIT.java
@@ -0,0 +1,30 @@
+package fr.efrei.studentserver.web.rest;
+
+import fr.efrei.studentserver.domain.Student;
+import fr.efrei.studentserver.repository.StudentRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@TestPropertySource(
+        locations = "classpath:application-test.properties")
+public class StudentRessourceIT {
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Test
+    @Transactional
+    void createItem() throws Exception {
+        int databaseSizeBeforeCreate = studentRepository.findAll().size();
+        assertThat(databaseSizeBeforeCreate).isEqualTo(0);
+
+//        List<Item> studentList = studentRepository.findAll();
+//        assertThat(studentList).hasSize(databaseSizeBeforeCreate + 1);
+    }
+}
