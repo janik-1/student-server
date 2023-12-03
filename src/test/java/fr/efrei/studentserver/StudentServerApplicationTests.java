a84aeef81b571b7cbfcf8ab5cd25d39355637a8b
janikJ <71125912+janik-1@users.noreply.github.com>
2023-12-03 23:19:09 +0100

    update studentTest

--- src/test/java/fr/efrei/studentserver/StudentServerApplicationTests.java
+++ src/test/java/fr/efrei/studentserver/StudentServerApplicationTests.java
@@ -2,10 +2,15 @@ package fr.efrei.studentserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(
+		locations = "classpath:application-test.properties")
 class StudentServerApplicationTests {
+
 	@Test
 	void contextLoads() {
 	}
+
 }
